713535c7d06dc370864e1f61155ccf98fee60135
Hanan Alhajri <92547643+HananAlhajri@users.noreply.github.com>
2024-03-24 11:42:25 +0300

    build: skeleton

---
+++ cap-be/src/test/java/com/demo/capmgnmt/CapitalManagementBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.capmgnmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitalManagementBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
